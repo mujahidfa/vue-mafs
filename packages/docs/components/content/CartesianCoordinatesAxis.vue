8c48d25015152cd7a56d57c99e7d0894f8f90639
Mujahid Anuar <17759705+mujahidfa@users.noreply.github.com>
2023-01-15 16:31:02 +0800

    Docs: Coordinates

---
+++ packages/docs/components/content/CartesianCoordinatesAxis.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { Mafs, CartesianCoordinates, labelPi } from "vue-mafs";
+
+function isOdd(n: number) {
+  return ((n % 2) + 2) % 2 === 0;
+}
+</script>
+
+<template>
+  <Mafs
+    :height="700"
+    :viewBox="{
+      x: [-8, 8],
+      y: [-Math.PI * 2, Math.PI * 2],
+      padding: Math.PI / 2,
+    }"
+    :preserveAspectRatio="false"
+  >
+    <CartesianCoordinates
+      :xAxis="{
+        lines: 1,
+        labels: (n) => (isOdd(n) ? n : ''),
+      }"
+      :yAxis="{
+        lines: Math.PI,
+        subdivisions: 4,
+        labels: labelPi,
+      }"
+    />
+  </Mafs>
+</template>
---
+++ packages/docs/components/content/CartesianCoordinatesBasic.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { Mafs, CartesianCoordinates } from "vue-mafs";
+</script>
+
+<template>
+  <Mafs :height="230">
+    <CartesianCoordinates />
+  </Mafs>
+</template>
