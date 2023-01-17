90eb7e1a16cb573ffa7876ad7c159c3b37122fc1
Mujahid Anuar <17759705+mujahidfa@users.noreply.github.com>
2023-01-17 23:52:08 +0800

    Docs(WIP): Lines

---
+++ packages/docs/components/content/LineSegmentExample.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import {
+  Mafs,
+  LineSegment,
+  CartesianCoordinates,
+  useMovablePoint,
+} from "vue-mafs";
+
+const point1 = useMovablePoint([-1, -1]);
+const point2 = useMovablePoint([2, 1]);
+</script>
+
+<template>
+  <Mafs :height="230" :viewBox="{ y: [-1, 1] }">
+    <CartesianCoordinates />
+    <LineSegment :point1="point1.point.value" :point2="point2.point.value" />
+    <point1.element.value />
+    <point1.element.value />
+  </Mafs>
+</template>
