50fa97fff3b4b494d92b04fca3eb5ec1b0d65e58
Mujahid Anuar <17759705+mujahidfa@users.noreply.github.com>
2023-01-21 21:57:57 +0800

    Docs: Polygon

---
+++ packages/docs/components/content/TrianglePool.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import {
+  Mafs,
+  Theme,
+  Polygon,
+  CartesianCoordinates,
+  useMovablePoint,
+} from "vue-mafs";
+
+const a = [2, 0] as [number, number];
+const b = [-2, 0] as [number, number];
+const c = useMovablePoint([0, 2]);
+</script>
+
+<template>
+  <Mafs :height="520">
+    <CartesianCoordinates />
+    <Polygon :points="[[c.x.value, -c.y.value], a, b]" strokeStyle="dashed" />
+    <Polygon :points="[c.point.value, a, b]" :color="Theme.blue" />
+    <c.element.value />
+  </Mafs>
+</template>
+
+<style>
+@import "vue-mafs/core.css";
+</style>
