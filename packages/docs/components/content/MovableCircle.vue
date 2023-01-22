1ce37ceb55c428944c7224357e980a91644dc637
Mujahid Anuar <17759705+mujahidfa@users.noreply.github.com>
2023-01-22 08:44:17 +0800

    Docs: Circle

---
+++ packages/docs/components/content/MovableCircle.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { computed } from "vue";
+import {
+  Mafs,
+  Circle,
+  CartesianCoordinates,
+  useMovablePoint,
+  vec,
+} from "vue-mafs";
+
+const pointOnCircle = useMovablePoint([Math.sqrt(2) / 2, Math.sqrt(2) / 2]);
+const r = computed(() => vec.mag(pointOnCircle.point.value));
+</script>
+
+<template>
+  <Mafs :height="495">
+    <CartesianCoordinates />
+    <Circle :center="[0, 0]" :radius="r" />
+    <pointOnCircle.element.value />
+  </Mafs>
+</template>
+
+<style>
+@import "vue-mafs/core.css";
+</style>
